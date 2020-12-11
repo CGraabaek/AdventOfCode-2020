91efdeca0f4806951228728e3fac92ae733f87af
Christian Graabæk Steenfeldt <c.graabaek@gmail.com>
2020-12-11 18:52:08 +0100

    Added Day 9 - Part 1

---
+++ Day_10/day_10.py
@@ -0,0 +1,55 @@
+from collections import Counter
+
+print("Advent Of Code - Day 10")
+
+
+PUZZLEINPUT = open('input.txt', 'r').read().split("\n")
+JOLT_ADAPTERS = [int(i) for i in PUZZLEINPUT]
+
+MAX_RATED_JOLT = max(JOLT_ADAPTERS)+3
+CHARGING_OUTLET = 0
+#Add max rated jolt and the outlet
+JOLT_ADAPTERS.append(MAX_RATED_JOLT)
+JOLT_ADAPTERS.append(CHARGING_OUTLET)
+
+JOLT_ADAPTERS = sorted(JOLT_ADAPTERS, reverse=False) 
+
+
+joltages = [JOLT_ADAPTERS[i+1]-JOLT_ADAPTERS[i] for i in range(len(JOLT_ADAPTERS)-1)]
+b = Counter(joltages)
+print(f'Part 1 {b[1]*b[3]}')
+
+
+def combs(a):
+    if len(a) == 0:
+        return [[]]
+    cs = []
+    for c in combs(a[1:]):
+        cs += [c, c+[a[0]]]
+    return cs
+
+def powerSet(items):
+    """
+    Power set generator: get all possible combinations of a list’s elements
+
+    Input:
+        items is a list
+    Output:
+        returns 2**n combination lists one at a time using a generator 
+
+    Reference: edx.org 6.00.2x Lecture 2 - Decision Trees and dynamic programming
+    """
+
+    N = len(items)
+    # enumerate the 2**N possible combinations
+    for i in range(2**N):
+        combo = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i >> j) % 2 == 1:
+                combo.append(items[j])
+        yield combo
+
+
+allcoms = combs(JOLT_ADAPTERS)
+print(allcoms)
---
+++ Day_9/day_9.py
@@ -0,0 +1,22 @@
+print("Advent Of Code - Day 9")
+
+PUZZLEINPUT = open('input.txt', 'r').read().split("\n")
+PUZZLEINPUT = [int(i) for i in PUZZLEINPUT]
+TAR = 0
+
+#PART 1
+for idx,number in enumerate(PUZZLEINPUT[25:]):
+    SUM_CHECK = PUZZLEINPUT[idx:idx+25]
+    target = PUZZLEINPUT[idx+25]
+    TEMP_SUM = set()
+    
+    for num in SUM_CHECK:
+        for number2 in SUM_CHECK: 
+            TEMP_SUM.add(num+number2)
+
+    if target not in TEMP_SUM:
+        TAR = target
+        print(f'Part 1: XMAS DATA Weak number is {target}')
+        break
+
+#PART 2
