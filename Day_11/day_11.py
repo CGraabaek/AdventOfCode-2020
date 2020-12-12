2cfa01f35e3a4df833a666982ff2f189a8c46abb
Christian Graabæk Steenfeldt <c.graabaek@gmail.com>
2020-12-12 15:54:11 +0100

    Started Day 11

--- Day_11/day_11.py
+++ Day_11/day_11.py
@@ -1,4 +1,20 @@
 print("Advent Of Code - Day 11")
 
+PUZZLEINPUT = open('test_input.txt', 'r').read().split("\n")
 
-PUZZLEINPUT = open('input.txt', 'r').read().split("\n")
+
+
+old_layout = {}
+for row_id, row in enumerate(PUZZLEINPUT):
+    for seat_id,seat in enumerate(row):
+        old_layout[row_id, seat_id] = seat
+
+# for idx,row in enumerate(PUZZLEINPUT):
+#     for seat_id,seat in enumerate(row):
+        
+#         print(f'SeatID {seat_id} - seat {seat}')
+#         if seat == 'L' and row[seat_id+1] == 'L' or row[seat_id+1] == '.':
+#             #print(PUZZLEINPUT[idx][seat_id])
+#             # PUZZLEINPUT[idx][seat_id] = '#'
+
+print(old_layout[2,6])
