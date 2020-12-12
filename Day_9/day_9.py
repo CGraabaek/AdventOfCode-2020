a4b06a8b5b7231c834b35d38de5440f545c6b6d8
Christian Graabæk Steenfeldt <c.graabaek@gmail.com>
2020-12-12 15:01:34 +0100

    Fixed Day 9, Part 2

--- Day_9/day_9.py
+++ Day_9/day_9.py
@@ -1,8 +1,9 @@
+import functools
 print("Advent Of Code - Day 9")
 
 PUZZLEINPUT = open('input.txt', 'r').read().split("\n")
 PUZZLEINPUT = [int(i) for i in PUZZLEINPUT]
-TAR = 0
+FINAL_TARGET = 0
 
 #PART 1
 for idx,number in enumerate(PUZZLEINPUT[25:]):
@@ -15,8 +16,15 @@ for idx,number in enumerate(PUZZLEINPUT[25:]):
             TEMP_SUM.add(num+number2)
 
     if target not in TEMP_SUM:
-        TAR = target
+        FINAL_TARGET = target
         print(f'Part 1: XMAS DATA Weak number is {target}')
         break
 
 #PART 2
+for start in range(len(PUZZLEINPUT)):
+    #The minimum sum has to be at least 2 numbers
+    for end in range(start+2, len(PUZZLEINPUT)):
+        sub_range = PUZZLEINPUT[start:end]
+        if sum(sub_range) == FINAL_TARGET: 
+            result2 = min(sub_range) + max(sub_range)
+            print(f"Found {result2} in {sub_range}")
